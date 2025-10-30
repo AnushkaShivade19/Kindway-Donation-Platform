aed754cd242e1dbdbe96bdd3e765ea9c6069b192
AnushkaShivade19 <shivadeanushka003@gmail.com>
2025-10-30 22:55:05 +0530

    Configure Neon PostgreSQL connection

--- kindway/settings.py
+++ kindway/settings.py
@@ -61,9 +61,12 @@ WSGI_APPLICATION = 'kindway.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 CSRF_TRUSTED_ORIGINS = ['https://*.vercel.app']
 
