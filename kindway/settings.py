8cff9c2f9c7f1a8bd362f00314d9bad56335a272
AnushkaShivade19 <shivadeanushka003@gmail.com>
2025-10-30 20:53:36 +0530

    Added production settings for Vercel

--- kindway/settings.py
+++ kindway/settings.py
@@ -82,7 +82,7 @@ USE_TZ = True
 # --- STATIC & MEDIA FILES ---
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # This is for WhiteNoise IN PRODUCTION ONLY
