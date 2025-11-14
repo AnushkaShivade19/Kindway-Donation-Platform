89d657bbebaa0063e47282e89fbbf6fc5b1f8291
AnushkaShivade19 <shivadeanushka003@gmail.com>
2025-11-14 23:31:56 +0530

    Done any

--- kindway/wsgi.py
+++ kindway/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kindway.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
+
+# --- ADD THIS LINE ---
+# Vercel looks for a variable named 'app'.
+app = application
